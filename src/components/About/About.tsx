960c6412cc933b398f527650bb00acbe0bf8d9ac
Sangmin Kang <kkang5430@naver.com>
2026-02-25 19:23:21 +0900

    About 페이지 완성

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,11 +2,12 @@ import {supabase} from "../../api/supabase.ts";
 import {useQuery} from "@tanstack/react-query";
 import type {Profile} from "../../types/Profile.ts";
 import styles from './About.module.css'
+import type {Experience} from "../../types/experience.ts";
 
 
 const About = () => {
 
-    const {data: profile, isLoading, error} = useQuery<Profile>({
+    const {data: profile, isLoading : isProfileLoading, error : pError} = useQuery<Profile>({
         queryKey: ['profile'],
         queryFn: async () => {
             const {data, error} = await supabase
@@ -18,23 +19,79 @@ const About = () => {
         }
     });
 
-    if (isLoading) return <p>로딩 중...</p>;
-    if (error) return <p>에러가 발생했습니다.</p>;
+    const {data: experience, isLoading : isExperienceLoading, error: eError} = useQuery<Experience[]>({
+        queryKey: ['experience'],
+        queryFn: async () => {
+            const {data, error} = await supabase
+                .from('experience')
+                .select('*')
+            if (error) throw error;
+            return data; // data 객체 profile 에 반환
+        }
+    });
+
+
+
+    if (isProfileLoading || isExperienceLoading) return <p>로딩 중...</p>;
+    if (pError || eError) return <p>에러가 발생했습니다.</p>;
 
 
     return (
 
         <section id="about" className={styles.section}>
-            <h1>안녕하세요</h1>
+            <h1 className={styles.title}>About</h1>
 
-            <img
-                src={profile?.profile_url}
-                style={{ width: '200px', borderRadius: '50%' }}
-            />
+            <div className={styles.content}>
+                <img
+                    src={profile?.profile_url} className={styles.profile_image}
+                />
 
-            <p>{profile?.name}</p>
-            <p>{profile?.dev_type}</p>
-            <p>{profile?.comment}</p>
+                <div className={styles.profile_info}>
+                    <p style={{fontSize: 48, marginBottom: 20, fontWeight: 'bold'}}>{profile?.name}</p>
+                    <p style={{fontSize: 20, marginBottom: 20, fontWeight: 'bold'}}>{profile?.dev_type}</p>
+                    <p style={{fontSize: 16, marginBottom: 70}}>{profile?.comment}</p>
+
+                    {/* 프로필 - 소개 아래의 experience 한 줄 */}
+                    <div className={styles.experience}>
+                        <div>
+                            <p style={{fontWeight: 'bold', fontSize: 24, marginBottom: 20}}>Activities</p>
+
+                            {experience
+                                ?.filter((item) => item.category === 'Activities') // 카테고리 Activities 만
+                                .map((item) => (
+                                    <p style={{fontSize: 16, marginBottom: 10}}>{item.content}</p>
+                                ))}
+                        </div>
+                        <div style={{marginLeft: 132}}>
+                            <p style={{fontWeight: 'bold', fontSize: 24, marginBottom: 20}}>Certifications</p>
+
+                            {experience
+                                ?.filter((item) => item.category === 'Certifications') // 카테고리 Certifications 만
+                                .map((item) => (
+                                    <p style={{fontSize: 16, marginBottom: 10}}>{item.content}</p>
+                                ))}
+                        </div>
+                    </div>
+
+                    <div className={styles.experience}>
+                        <div>
+                            <p style={{fontWeight: 'bold', fontSize: 24, marginBottom: 20}}>Location</p>
+
+                            <p style={{fontSize: 16, marginBottom: 10}}>{profile?.location}</p>
+
+                        </div>
+                        <div style={{marginLeft: 248}}>
+                            <p style={{fontWeight: 'bold', fontSize: 24, marginBottom: 20}}>Birth</p>
+
+                            <p style={{fontSize: 16, marginBottom: 10}}>{profile?.birth_date}</p>
+
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
         </section>
     );
 };
---
+++ src/types/experience.ts
@@ -0,0 +1,6 @@
+export interface Experience {
+    id: number;
+    category: string;
+    content: string;
+    created_at: string;
+}
